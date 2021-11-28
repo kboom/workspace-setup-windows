19813f8cafee278ad4ccda43fca57eee4c00b3c3
Grzegorz Gurgul <ggurgul@microsoft.com>
2021-11-28 16:54:26 +0100

    more

--- PowerShell/Install-Profile.ps1
+++
@@ -1,6 +0,0 @@
-# This script installs required tools
-Install-Module oh-my-posh -Scope CurrentUser
-Get-PoshThemes
-Import-Module -Name Terminal-Icons
-Install-Module -Name Terminal-Icons -Repository PSGallery
-Set-PoshPrompt -Theme jandedobbeleer
--- PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,3 +0,0 @@
-# This file initializes your profile on every session startup
-Import-Module -Name Terminal-Icons
-Set-PoshPrompt -Theme "$PSScriptRoot\ggurgul-oh-my-posh-profile.json"
